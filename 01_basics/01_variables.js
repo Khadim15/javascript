e978809c9a56a19e70373dff1dcafa11ee46b1fa
Khadim Rasool <112551114+Khadim15@users.noreply.github.com>
2024-10-22 18:40:39 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144567
+let accountEmail = "khadim@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+accountEmail = "k@google.com"
+accountPassword = "fuhfg"
+accountCity = "lko"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
